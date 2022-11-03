94c28becc38e1473c390040234bc98e7ba26d97a
Aimen Anwaar <aimenanwaar13@gmail.com>
2022-11-04 02:21:59 +0500

    version 1.0

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -9,6 +9,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:wordle_game/event_bus.dart';
+import 'package:wordle_game/theme.dart';
 import 'package:wordle_game/validation_provider.dart';
 import 'package:wordle_game/display_pannel.dart';
 import 'package:wordle_game/instruction_pannel.dart';
@@ -70,89 +71,21 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
       children: [
         Positioned.fill(
           child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).brightness == Brightness.dark
+            appBar: appbar(context),
+            body: Container(
+              color: Theme.of(context).brightness == Brightness.dark
                   ? Colors.grey[850]
                   : Colors.white,
-              elevation: 0.0,
-              shadowColor: Colors.transparent,
-              toolbarHeight: 80.0,
-              titleTextStyle: TextStyle(
-                color: Theme.of(context).brightness == Brightness.dark
-                    ? Colors.grey[100]
-                    : Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-              ),
-              title: const Text('WORDLE'),
-              centerTitle: true,
-              //iconTheme: const IconThemeData(color: Colors.black),
-              actions: [
-                AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 750),
-                  reverseDuration: const Duration(milliseconds: 750),
-                  switchInCurve: Curves.bounceOut,
-                  switchOutCurve: Curves.bounceIn,
-                  transitionBuilder: (child, animation) {
-                    var rotateAnimation =
-                        Tween<double>(begin: 0, end: 2 * pi).animate(animation);
-                    var opacAnimation =
-                        Tween<double>(begin: 0, end: 1).animate(animation);
-                    return AnimatedBuilder(
-                      animation: rotateAnimation,
-                      builder: (context, child) {
-                        return Transform(
-                          transform: Matrix4.rotationZ(
-                              rotateAnimation.status == AnimationStatus.reverse
-                                  ? 2 * pi - rotateAnimation.value
-                                  : rotateAnimation.value),
-                          alignment: Alignment.center,
-                          child: Opacity(
-                            opacity: opacAnimation.value,
-                            child: child,
-                          ),
-                        );
-                      },
-                      child: child,
-                    );
-                  },
-                  child: IconButton(
-                    key: ValueKey(mode),
-                    icon: mode == Brightness.light
-                        ? const Icon(Icons.dark_mode_outlined)
-                        : const Icon(Icons.dark_mode),
-                    onPressed: () =>
-                        mainBus.emit(event: "ToggleTheme", args: []),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.help_outline_outlined),
-                  //color: Colors.black,
-                  onPressed: () {
-                    showInstructionDialog(context: context);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.refresh_rounded),
-                  //color: Colors.black,
-                  onPressed: () {
-                    mainBus.emit(event: "NewGame", args: []);
-                  },
-                ),
-              ],
-            ),
-            body: Container(
               child: ValidationProvider(
                 database: widget.database,
                 wordLen: widget.wordLen,
                 maxChances: widget.maxChances,
                 gameMode: widget.gameMode,
                 child: WordleDisplayWidget(
-                    wordLen: widget.wordLen, maxChances: widget.maxChances),
+                  wordLen: widget.wordLen,
+                  maxChances: widget.maxChances,
+                ),
               ),
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? Colors.grey[850]
-                  : Colors.white,
             ),
           ),
         ),
